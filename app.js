7cfb79904a0b373ea3a0174e7b28b8c3dcec7e09
iqbalalayyubi <iqbal.is415@gmail.com>
2022-09-14 22:12:39 +0700

    ubah env port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = process.env.port || 5000
+const port = process.env.PORT || 5000
 const User = require('./config')
 app.use(express.json())
 app.use(cors())
